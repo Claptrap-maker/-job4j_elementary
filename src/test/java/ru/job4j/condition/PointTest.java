c84f79670d82af3acadae4af112017537a52ec99
Claptrap-maker <lelik575435@gmail.com>
2023-07-04 20:10:07 +0300

    3. Тесты для определения расстояния между точками. [#159459]

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when43to72then3dot16() {
+        double expected = 3.16;
+        int x1 = -4;
+        int y1 = 3;
+        int x2 = -7;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when28to41then10dot81() {
+        double expected = 10.81;
+        int x1 = 2;
+        int y1 = 8;
+        int x2 = -4;
+        int y2 = -1;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when21to04then5dot38() {
+        double expected = 5.38;
+        int x1 = 2;
+        int y1 = -1;
+        int x2 = 0;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when52to34then10() {
+        double expected = 10;
+        int x1 = -5;
+        int y1 = 2;
+        int x2 = 3;
+        int y2 = -4;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
